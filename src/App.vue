7786dfc5124c7d4b83bbdccf85a038b98bd4037c
locemarn <marcelon27@gmail.com>
2018-02-07 12:59:27 -0200

    oi

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <app-content></app-content>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import AppHeader from './layout/AppHeader.vue'
+import AppContent from './layout/AppContent.vue'
+import AppFooter from './layout/AppFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader, AppContent, AppFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/layout/AppFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <h3>Footer</h3>
+</template>
+
+<script>
+export default {
+
+}
+</script>
