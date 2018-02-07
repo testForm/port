3af2601aa904fa1a34ed0a5b04b846e6f4b02941
locemarn <marcelon27@gmail.com>
2018-02-07 13:14:44 -0200

    1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <app-header></app-header>
+    <app-content></app-content>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import AppHeader from './layout/AppHeader'
+import AppContent from './layout/AppContent'
+import AppFooter from './layout/AppFooter'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppHeader, AppContent, AppFooter
   }
 }
 </script>
